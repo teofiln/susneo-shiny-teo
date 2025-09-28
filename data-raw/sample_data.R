f546aff7baae9eda7ac9bfbab79cea065dc553b3
teofiln <teofiln@gmail.com>
2025-09-28 15:08:47 -0600

    portable file name

--- data-raw/sample_data.R
+++ data-raw/sample_data.R
@@ -1,5 +1,5 @@
 # Prepare sample dataset as internal data of the package
 # Stores the dataset in data/sample_data.rda
 # and documents it in R/sample_data.R
-sample_data <- read.csv("inst/extdata/SAMPLE ASSIGNMENT DATA.csv")
+sample_data <- read.csv("inst/extdata/sample_data.csv")
 usethis::use_data(sample_data, overwrite = TRUE)
