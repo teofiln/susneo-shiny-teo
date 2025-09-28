ca8212280af6700e63d05f04e8861b031f97ea0f
teofiln <teofiln@gmail.com>
2025-09-28 16:27:28 -0600

    set logging threshold for dev

--- dev/run_dev.R
+++ dev/run_dev.R
@@ -1,5 +1,5 @@
 # Set options here
-options(golem.app.prod = FALSE) # TRUE = production mode, FALSE = development mode
+options(golem.app.prod = FALSE)
 
 # Comment this if you don't want the app to be served on a random port
 options(shiny.port = 5000)
@@ -13,6 +13,8 @@ golem::detach_all_attached()
 
 # Document and reload your package
 golem::document_and_reload()
+
+logger::log_threshold(logger::DEBUG)
 
 # Run the application
 run_app()
