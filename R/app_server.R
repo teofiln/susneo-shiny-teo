c49457aa0a62219b7f4e748a0d3a982cae8c4224
teofiln <teofiln@gmail.com>
2025-09-28 21:30:08 -0600

    r cmd check notes

--- R/app_server.R
+++ R/app_server.R
@@ -3,6 +3,8 @@
 #' @param input,output,session Internal parameters for {shiny}.
 #'     DO NOT REMOVE.
 #' @import shiny
+#' @importFrom stats runif
+#' @importFrom utils read.csv
 #' @noRd
 app_server <- function(input, output, session) {
   mod_data_upload_server("data_upload")
